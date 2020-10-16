a8b0f9009be69773b0a81c0561b554ef3ec3fc19
nbuikema <46764298+nbuikema@users.noreply.github.com>
2020-10-16 11:57:49 -0500

    initial react client config with CRA, AntDesign, and dummy routes

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Header from './components/nav/Header';
+
+import Home from './pages/core/Home';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </>
+  );
+};
+
+export default App;
