e137d21666c0fc3b5b94cd007ef18095c91ad82c
nbuikema <46764298+nbuikema@users.noreply.github.com>
2020-11-02 13:58:17 -0600

    finished order analytics

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -8,7 +8,8 @@ import {
   YAxis,
   Legend,
   AreaChart,
-  Area
+  Area,
+  ResponsiveContainer
 } from 'recharts';
 import { getProductsBySoldValue } from '../../api/product';
 import { getOrdersByDate } from '../../api/order';
@@ -49,7 +50,7 @@ const AdminDashboard = () => {
         <div className="col-md-2">
           <AdminNav />
         </div>
-        <div className="col">
+        <div className="col-md-10">
           <h4>Admin Dashboard</h4>
           <hr />
           <Tabs defaultActiveKey="1" type="card" size="large">
@@ -72,16 +73,24 @@ const AdminDashboard = () => {
                   </Select>
                 </span>
               </h5>
-              <BarChart width={500} height={300} data={products}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="title" />
-                <YAxis yAxisId="left" orientation="left" stroke="#8884d8" />
-                <YAxis yAxisId="right" orientation="right" stroke="#82ca9d" />
-                <Tooltip />
-                <Legend />
-                <Bar yAxisId="left" dataKey="totalPrice" fill="#8884d8" />
-                <Bar yAxisId="right" dataKey="quantitySold" fill="#82ca9d" />
-              </BarChart>
+              <ResponsiveContainer width="100%" height={400}>
+                <BarChart data={products}>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="title" />
+                  <YAxis yAxisId="left" orientation="left" stroke="#8884d8" />
+                  <YAxis yAxisId="right" orientation="right" stroke="#82ca9d" />
+                  <Tooltip
+                    formatter={(value, name) => {
+                      return name === 'totalPrice'
+                        ? `$${new Intl.NumberFormat('en').format(value)}`
+                        : value;
+                    }}
+                  />
+                  <Legend />
+                  <Bar yAxisId="left" dataKey="quantitySold" fill="#8884d8" />
+                  <Bar yAxisId="right" dataKey="totalPrice" fill="#82ca9d" />
+                </BarChart>
+              </ResponsiveContainer>
             </TabPane>
             <TabPane tab="Orders" key="Orders">
               <h5>
@@ -102,26 +111,65 @@ const AdminDashboard = () => {
                   </Select>
                 </span>
               </h5>
-              <BarChart width={500} height={300} data={orders}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="dateGroup" />
-                <YAxis yAxisId="left" orientation="left" stroke="#8884d8" />
-                <Tooltip />
-                <Legend />
-                <Bar yAxisId="left" dataKey="numOrders" fill="#8884d8" />
-              </BarChart>
-              <AreaChart width={500} height={400} data={orders}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="dateGroup" />
-                <YAxis />
-                <Tooltip />
-                <Area
-                  type="monotone"
-                  dataKey="numOrders"
-                  stroke="#8884d8"
-                  fill="#8884d8"
-                />
-              </AreaChart>
+              <ResponsiveContainer width="100%" height={400}>
+                <BarChart data={orders}>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="dateGroup" />
+                  <YAxis yAxisId="left" orientation="left" stroke="#8884d8" />
+                  <YAxis yAxisId="right" orientation="right" stroke="#82ca9d" />
+                  <Tooltip
+                    formatter={(value, name) => {
+                      return name === 'average'
+                        ? `$${new Intl.NumberFormat('en').format(value)}`
+                        : value;
+                    }}
+                  />
+                  <Legend />
+                  <Bar yAxisId="left" dataKey="orders" fill="#8884d8" />
+                  <Bar yAxisId="right" dataKey="average" fill="#82ca9d" />
+                </BarChart>
+              </ResponsiveContainer>
+              <ResponsiveContainer width="100%" height={400}>
+                <AreaChart data={orders}>
+                  <defs>
+                    <linearGradient id="orders" x1="0" y1="0" x2="0" y2="1">
+                      <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                      <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                    </linearGradient>
+                    <linearGradient id="average" x1="0" y1="0" x2="0" y2="1">
+                      <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                      <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                    </linearGradient>
+                  </defs>
+                  <XAxis dataKey="name" />
+                  <YAxis yAxisId="left" orientation="left" stroke="#8884d8" />
+                  <YAxis yAxisId="right" orientation="right" stroke="#82ca9d" />
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <Tooltip
+                    formatter={(value, name) => {
+                      return name === 'average'
+                        ? `$${new Intl.NumberFormat('en').format(value)}`
+                        : value;
+                    }}
+                  />
+                  <Area
+                    type="monotone"
+                    dataKey="orders"
+                    stroke="#8884d8"
+                    fillOpacity={1}
+                    fill="url(#orders)"
+                    yAxisId="left"
+                  />
+                  <Area
+                    type="monotone"
+                    dataKey="average"
+                    stroke="#82ca9d"
+                    fillOpacity={1}
+                    fill="url(#average)"
+                    yAxisId="right"
+                  />
+                </AreaChart>
+              </ResponsiveContainer>
             </TabPane>
           </Tabs>
         </div>
--- server/controllers/order.js
+++ server/controllers/order.js
@@ -25,11 +25,17 @@ exports.getOrdersByDate = async (req, res) => {
     sort = { orderedMonth: 1 };
   }
   if (date === 'This Month') {
-    query = { orderedMonth: { $eq: new Date().getMonth() + 1 } };
+    query = {
+      orderedMonth: { $eq: new Date().getMonth() + 1 },
+      orderedYear: { $eq: new Date().getFullYear() }
+    };
     sort = { orderedDayOfMonth: 1 };
   }
   if (date === 'This Week') {
-    query = { orderedWeek: { $eq: moment(new Date()).week() - 1 } };
+    query = {
+      orderedWeek: { $eq: moment(new Date()).week() - 1 },
+      orderedYear: { $eq: new Date().getFullYear() }
+    };
     sort = { orderedDayOfWeek: 1 };
   }
   if (date === 'Today') {
@@ -48,7 +54,8 @@ exports.getOrdersByDate = async (req, res) => {
         orderedDayOfMonth: { $dayOfMonth: '$createdAt' },
         orderedYear: { $year: '$createdAt' },
         orderedWeek: { $week: '$createdAt' },
-        orderedDayOfWeek: { $dayOfWeek: '$createdAt' }
+        orderedDayOfWeek: { $dayOfWeek: '$createdAt' },
+        orderedHour: { $hour: '$createdAt' }
       }
     },
     { $match: query },
@@ -57,9 +64,80 @@ exports.getOrdersByDate = async (req, res) => {
 
   let ordersByDate = [];
 
+  if (date === 'This Year') {
+    ordersByDate = [
+      { dateGroup: 'January', orders: 0, average: 0 },
+      { dateGroup: 'February', orders: 0, average: 0 },
+      { dateGroup: 'March', orders: 0, average: 0 },
+      { dateGroup: 'April', orders: 0, average: 0 },
+      { dateGroup: 'May', orders: 0, average: 0 },
+      { dateGroup: 'June', orders: 0, average: 0 },
+      { dateGroup: 'July', orders: 0, average: 0 },
+      { dateGroup: 'August', orders: 0, average: 0 },
+      { dateGroup: 'September', orders: 0, average: 0 },
+      { dateGroup: 'October', orders: 0, average: 0 },
+      { dateGroup: 'November', orders: 0, average: 0 },
+      { dateGroup: 'December', orders: 0, average: 0 }
+    ];
+  }
+
+  if (date === 'This Month') {
+    ordersByDate = [
+      {
+        dateGroup: `${orders[0].orderedMonth}/1 - ${orders[0].orderedMonth}/7`,
+        orders: 0,
+        average: 0
+      },
+      {
+        dateGroup: `${orders[0].orderedMonth}/8 - ${orders[0].orderedMonth}/14`,
+        orders: 0,
+        average: 0
+      },
+      {
+        dateGroup: `${orders[0].orderedMonth}/15 - ${orders[0].orderedMonth}/21`,
+        orders: 0,
+        average: 0
+      },
+      {
+        dateGroup: `${orders[0].orderedMonth}/22 - ${orders[0].orderedMonth}/28`,
+        orders: 0,
+        average: 0
+      },
+      {
+        dateGroup: `${orders[0].orderedMonth}/29 - ${orders[0].orderedMonth}/31`,
+        orders: 0,
+        average: 0
+      }
+    ];
+  }
+
+  if (date === 'This Week') {
+    ordersByDate = [
+      { dateGroup: 'Sunday', orders: 0, average: 0 },
+      { dateGroup: 'Monday', orders: 0, average: 0 },
+      { dateGroup: 'Tuesday', orders: 0, average: 0 },
+      { dateGroup: 'Wednesday', orders: 0, average: 0 },
+      { dateGroup: 'Thursday', orders: 0, average: 0 },
+      { dateGroup: 'Friday', orders: 0, average: 0 },
+      { dateGroup: 'Saturday', orders: 0, average: 0 }
+    ];
+  }
+
+  if (date === 'Today') {
+    ordersByDate = [
+      { dateGroup: '12 am - 4 am EST', orders: 0, average: 0 },
+      { dateGroup: '4 am - 8 am EST', orders: 0, average: 0 },
+      { dateGroup: '8 am - 12 pm EST', orders: 0, average: 0 },
+      { dateGroup: '12 pm - 4 pm EST', orders: 0, average: 0 },
+      { dateGroup: '4 pm - 8 pm EST', orders: 0, average: 0 },
+      { dateGroup: '8 pm - 12 am EST', orders: 0, average: 0 }
+    ];
+  }
+
   orders.forEach((order) => {
     let dateGroup = '';
-    let numOrders = 0;
+    let orders = 0;
+    let average = 0;
 
     if (date === 'Overall') {
       const existsIndex = ordersByDate.findIndex(
@@ -67,32 +145,28 @@ exports.getOrdersByDate = async (req, res) => {
       );
 
       if (existsIndex > -1) {
-        ordersByDate[existsIndex].numOrders++;
+        ordersByDate[existsIndex].orders++;
+        ordersByDate[existsIndex].average += order.paymentIntent.amount / 100;
       } else {
         dateGroup = order.orderedYear;
-        numOrders = 1;
+        orders = 1;
+        average = order.paymentIntent.amount / 100;
 
-        ordersByDate.push({ dateGroup, numOrders });
+        ordersByDate.push({ dateGroup, orders, average });
       }
     }
 
     if (date === 'This Year') {
-      const existsIndex = ordersByDate.findIndex(
+      const index = ordersByDate.findIndex(
         (x) => x.dateGroup === moment(order.orderedMonth, 'MM').format('MMMM')
       );
 
-      if (existsIndex > -1) {
-        ordersByDate[existsIndex].numOrders++;
-      } else {
-        dateGroup = moment(order.orderedMonth, 'MM').format('MMMM');
-        numOrders = 1;
-
-        ordersByDate.push({ dateGroup, numOrders });
-      }
+      ordersByDate[index].orders++;
+      ordersByDate[index].average += order.paymentIntent.amount / 100;
     }
 
     if (date === 'This Month') {
-      const existsIndex = ordersByDate.findIndex((x) => {
+      const exists = ordersByDate.findIndex((x) => {
         let search = '';
 
         if (order.orderedDayOfMonth <= 7) {
@@ -114,50 +188,55 @@ exports.getOrdersByDate = async (req, res) => {
         return x.dateGroup === search;
       });
 
-      if (existsIndex > -1) {
-        ordersByDate[existsIndex].numOrders++;
-      } else {
-        if (order.orderedDayOfMonth <= 7) {
-          dateGroup = `${order.orderedMonth}/1 - ${order.orderedMonth}/7`;
-        }
-        if (order.orderedDayOfMonth >= 8 && order.orderedDayOfMonth <= 14) {
-          dateGroup = `${order.orderedMonth}/8 - ${order.orderedMonth}/14`;
-        }
-        if (order.orderedDayOfMonth >= 15 && order.orderedDayOfMonth <= 21) {
-          dateGroup = `${order.orderedMonth}/15 - ${order.orderedMonth}/21`;
-        }
-        if (order.orderedDayOfMonth >= 22 && order.orderedDayOfMonth <= 28) {
-          dateGroup = `${order.orderedMonth}/1 - ${order.orderedMonth}/8`;
-        }
-        if (order.orderedDayOfMonth >= 29) {
-          dateGroup = `${order.orderedMonth}/29 - ${order.orderedMonth + 1}/1`;
-        }
-        numOrders = 1;
-
-        ordersByDate.push({ dateGroup, numOrders });
-      }
+      ordersByDate[exists].orders++;
+      ordersByDate[exists].average += order.paymentIntent.amount / 100;
     }
 
     if (date === 'This Week') {
-      const existsIndex = ordersByDate.findIndex(
+      const exists = ordersByDate.findIndex(
         (x) =>
-          x.dateGroup === moment(order.orderedDayOfWeek - 1, 'E').format('dddd')
+          x.dateGroup === moment(order.orderedDayOfWeek - 2, 'e').format('dddd')
       );
 
-      if (existsIndex > -1) {
-        ordersByDate[existsIndex].numOrders++;
-      } else {
-        dateGroup = moment(order.orderedDayOfWeek - 1, 'E').format('dddd');
-        numOrders = 1;
+      ordersByDate[exists].orders++;
+      ordersByDate[exists].average += order.paymentIntent.amount / 100;
+    }
 
-        ordersByDate.push({ dateGroup, numOrders });
+    if (date === 'Today') {
+      if (order.orderedHour - 5 >= 0 && order.orderedHour - 5 <= 3) {
+        ordersByDate[0].orders++;
+        ordersByDate[0].average += order.paymentIntent.amount / 100;
+      }
+      if (order.orderedHour - 5 >= 4 && order.orderedHour - 5 <= 7) {
+        ordersByDate[1].orders++;
+        ordersByDate[1].average += order.paymentIntent.amount / 100;
+      }
+      if (order.orderedHour - 5 >= 8 && order.orderedHour - 5 <= 11) {
+        ordersByDate[2].orders++;
+        ordersByDate[2].average += order.paymentIntent.amount / 100;
+      }
+      if (order.orderedHour - 5 >= 12 && order.orderedHour - 5 <= 15) {
+        ordersByDate[3].orders++;
+        ordersByDate[3].average += order.paymentIntent.amount / 100;
+      }
+      if (order.orderedHour - 5 >= 16 && order.orderedHour - 5 <= 19) {
+        ordersByDate[4].orders++;
+        ordersByDate[4].average += order.paymentIntent.amount / 100;
+      }
+      if (order.orderedHour - 5 >= 20 && order.orderedHour - 5 <= 23) {
+        ordersByDate[5].orders++;
+        ordersByDate[5].average += order.paymentIntent.amount / 100;
       }
     }
   });
 
-  if (date === 'Today') {
-    ordersByDate.push({ dateGroup: 'Today', numOrders: orders.length });
-  }
+  ordersByDate.forEach((section) => {
+    section.average === 0
+      ? 0
+      : (section.average = parseFloat(
+          (section.average / section.orders).toFixed(2)
+        ));
+  });
 
   res.json(ordersByDate);
 };
